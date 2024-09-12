6c16bf9e4b8ce0fb69c3e6fceca24e33e200b84e
JwaSangHeon <whk4051@naver.com>
2024-09-12 15:14:58 +0900

    clerk & convex 연결 및 로그인 로딩 component 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click me!</Button>;
+  return (
+    <div className="flex gap-x-4">
+      <div>로그인 된 유저만 접근 가능한 화면입니다</div>
+      <UserButton />
+    </div>
+  );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://darling-porpoise-13.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
